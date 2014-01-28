1e0f98f4e8640ef4970aa642e8be9c893015bd81
Trenton D. Adams <trent@trentonadams.ca>
2014-01-27 20:01:34 -0700

    - refactor work item related tasks into a single javascript object

--- src/main/webapp/js/aceio.js
+++ src/main/webapp/js/aceio.js
@@ -8,29 +8,146 @@ var aceGetWorkItemsUrl;
 var aceLoginUrl;
 var guid;
 
-var workList = [
+var myWork = {
+    /**
+     * Loads the task items for each week.
+     */
+    loadWeeks: function loadMyWeeks()
     {
-        weekStart: new Date(),
-        approvalStatus: '',
-        approvalStatusName: '',
-        project: '',
-        projectName: '',
-        taskId: '',
-        taskName: '',
-        timeSheetLineId: '',
-        timeSheetPeriodId: '',
-        work: {
-            sun: '',
-            mon: '',
-            tue: '',
-            wed: '',
-            thu: '',
-            fri: '',
-            sat: '',
-            total: ''
+        jQuery.ajax({
+            url: aceGetWeeksUrl,
+            type: 'get',
+            dataType: 'json',
+            data: 'guid=' + guid,
+            success: function (page, status, jqXHR)
+            {
+                log('my weeks: %o', page);
+                myWork.list = [];
+                myWork.load(page);
+            },
+            error: function (page, status, jqXHR)
+            {
+                aceIOError(page, status, jqXHR);
+            }
+        });
+    },
+    /**
+     * Loads the in progress work list into workList global variable.
+     */
+    load: function (workWeek)
+    {
+        for (i = 0; i < workWeek.results.length; i++)
+        {
+            jQuery.ajax({
+                url: aceGetWorkItemsUrl,
+                type: 'get',
+                dataType: 'json',
+                data: 'guid=' + guid + '&timeperiodid=' +
+                        workWeek.results[i].TIMESHEET_PERIOD_ID,
+                workListGenerator: true,
+                success: function (page, status, jqXHR)
+                {
+                    for (i = 0; i < page.results.length; i++)
+                    {
+                        var workItem = page.results[i];
+                        log('work item %o', workItem);
+                        workWeekStart =
+                                new Date(+/\/Date\((\d*)\)\//.exec(workItem.DATE_WEEK_START)[1] +
+                                        new Date().getTimezoneOffset() * 60 *
+                                                1000);
+                        log('date: %s', workWeekStart);
+                        var newWorkItem = {
+                            weekStart: workWeekStart,
+                            approvalStatus: workItem.APPROVAL_STATUS,
+                            approvalStatusName: workItem.APPROVAL_STATUS_NAME,
+                            project: workItem.PROJECT_ID,
+                            projectName: workItem.PROJECT_NAME,
+                            taskId: workItem.TASK_ID,
+                            taskName: workItem.TASK_RESUME,
+                            timeSheetLineId: workItem.TIMESHEET_LINE_ID,
+                            timeSheetPeriodId: workItem.TIMESHEET_PERIOD_ID,
+                            work: {
+                                sun: workItem.TOTAL1,
+                                mon: workItem.TOTAL2,
+                                tue: workItem.TOTAL3,
+                                wed: workItem.TOTAL4,
+                                thu: workItem.TOTAL5,
+                                fri: workItem.TOTAL6,
+                                sat: workItem.TOTAL7,
+                                total: workItem.TOTAL
+                            }
+                        };
+
+                        if (0 != newWorkItem.timeSheetLineId)
+                        { // only *real* time items, not the predicted ones.
+                            myWork.list.push(newWorkItem);
+                            var workRow = '' +
+                                    '            <tr>' +
+                                    '            <td>' +
+                                    newWorkItem.approvalStatusName +
+                                    '</td>' +
+                                    '            <td>' +
+                                    newWorkItem.projectName +
+                                    '</td>' +
+                                    '            <td>' + newWorkItem.taskName +
+                                    '</td>';
+                            workRow += '<td>'
+                            workRow += newWorkItem.work.sun + '-';
+                            workRow += newWorkItem.work.mon + '-';
+                            workRow += newWorkItem.work.tue + '-';
+                            workRow += newWorkItem.work.wed + '-';
+                            workRow += newWorkItem.work.thu + '-';
+                            workRow += newWorkItem.work.fri + '-';
+                            workRow += newWorkItem.work.sat;
+                            workRow += '</td>';
+                            workRow += '            </tr>'
+                            jQuery('#time').append(workRow);
+                        }
+                    }
+                },
+                error: function (page, status, jqXHR)
+                {
+                    aceIOError(page, status, jqXHR);
+                }
+            });
         }
-    }
-];
+    },
+    ajaxStop: function ()
+    {
+        log('work list: %o', myWork.list);
+        /* CRITICAL We could iterate through this list, to find a project and work
+         * item that matches the time frame, and just add to it's hours for the
+         * day of the week, as well as the comment. */
+        if (0 < myWork.list.length)
+        {
+            jQuery('#work').show();
+        }
+    },
+    list: [
+        {
+            weekStart: new Date(),
+            weekEnd: new Date(),
+            approvalStatus: '',
+            approvalStatusName: '',
+            project: '',
+            projectName: '',
+            taskId: '',
+            taskName: '',
+            timeSheetLineId: '',
+            timeSheetPeriodId: '',
+            work: {
+                sun: '',
+                mon: '',
+                tue: '',
+                wed: '',
+                thu: '',
+                fri: '',
+                sat: '',
+                total: ''
+            }
+        }
+    ]
+};
 
 /***
  * Uses account info returned through a query to either the ace login, or
@@ -54,7 +171,7 @@ function aceLogin(page, status, jqXHR)
                 "fasttime=" + loginInfo.GUID + "; path=/fasttime";
         jQuery('#login').hide();
         guid = loginInfo.GUID;
-        loadMyWeeks();
+        myWork.loadWeeks();
     }
     else
     {
@@ -88,14 +205,7 @@ jQuery(document).ready(function ()
 {
     jQuery(document).ajaxStop(function (event, XMLHttpRequest, ajaxOptions)
     {
-        log('work list: %o', workList);
-        /* CRITICAL We could iterate through this list, to find a project and work
-         * item that matches the time frame, and just add to it's hours for the
-         * day of the week, as well as the comment. */
-        if (0 < workList.length)
-        {
-            jQuery('#work').show();
-        }
+        myWork.ajaxStop();
     });
 
     // Check for existing login
@@ -146,108 +256,6 @@ jQuery(document).ready(function ()
             });
 });
 
-/**
- * Loads the in progress work list into workList global variable.
- */
-function loadMyWorkItems(workWeek)
-{
-    for (i = 0; i < workWeek.results.length; i++)
-    {
-        jQuery.ajax({
-            url: aceGetWorkItemsUrl,
-            type: 'get',
-            dataType: 'json',
-            data: 'guid=' + guid + '&timeperiodid=' +
-                    workWeek.results[i].TIMESHEET_PERIOD_ID,
-            workListGenerator: true,
-            success: function (page, status, jqXHR)
-            {
-                for (i = 0; i < page.results.length; i++)
-                {
-                    var workItem = page.results[i];
-                    log('work item %o', workItem);
-                    workWeekStart =
-                            new Date(+/\/Date\((\d*)\)\//.exec(workItem.DATE_WEEK_START)[1] +
-                                    new Date().getTimezoneOffset() * 60 * 1000);
-                    log('date: %s', workWeekStart);
-                    var newWorkItem = {
-                        weekStart: workWeekStart,
-                        approvalStatus: workItem.APPROVAL_STATUS,
-                        approvalStatusName: workItem.APPROVAL_STATUS_NAME,
-                        project: workItem.PROJECT_ID,
-                        projectName: workItem.PROJECT_NAME,
-                        taskId: workItem.TASK_ID,
-                        taskName: workItem.TASK_RESUME,
-                        timeSheetLineId: workItem.TIMESHEET_LINE_ID,
-                        timeSheetPeriodId: workItem.TIMESHEET_PERIOD_ID,
-                        work: {
-                            sun: workItem.TOTAL1,
-                            mon: workItem.TOTAL2,
-                            tue: workItem.TOTAL3,
-                            wed: workItem.TOTAL4,
-                            thu: workItem.TOTAL5,
-                            fri: workItem.TOTAL6,
-                            sat: workItem.TOTAL7,
-                            total: workItem.TOTAL
-                        }
-                    };
-
-                    if (0 != newWorkItem.timeSheetLineId)
-                    { // only *real* time items, not the predicted ones.
-                        workList.push(newWorkItem);
-                        var workRow = '' +
-                                '            <tr>' +
-                                '            <td>' +
-                                newWorkItem.approvalStatusName +
-                                '</td>' +
-                                '            <td>' + newWorkItem.projectName +
-                                '</td>' +
-                                '            <td>' + newWorkItem.taskName +
-                                '</td>';
-                        workRow += '<td>'
-                        workRow += newWorkItem.work.sun + '-';
-                        workRow += newWorkItem.work.mon + '-';
-                        workRow += newWorkItem.work.tue + '-';
-                        workRow += newWorkItem.work.wed + '-';
-                        workRow += newWorkItem.work.thu + '-';
-                        workRow += newWorkItem.work.fri + '-';
-                        workRow += newWorkItem.work.sat;
-                        workRow += '</td>';
-                        workRow += '            </tr>'
-                        jQuery('#time').append(workRow);
-                    }
-                }
-            },
-            error: function (page, status, jqXHR)
-            {
-                aceIOError(page, status, jqXHR);
-            }
-        });
-    }
-}
-
-/**
- * Loads the task items for each week.
- */
-function loadMyWeeks()
-{
-    jQuery.ajax({
-        url: aceGetWeeksUrl,
-        type: 'get',
-        dataType: 'json',
-        data: 'guid=' + guid,
-        success: function (page, status, jqXHR)
-        {
-            log('my weeks: %o', page);
-            workList = [];
-            loadMyWorkItems(page);
-        },
-        error: function (page, status, jqXHR)
-        {
-            aceIOError(page, status, jqXHR);
-        }
-    });
-}
 /**
  * Pass a url, and a test call is created, and the results logged.  guid is
  * automatically appended.  This allows playing around in the chrome developer
