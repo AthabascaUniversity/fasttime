5d139592476cbc04c9f7adafcf380b7989062316
Trenton D. Adams <trent@trentonadams.ca>
2014-01-27 19:45:21 -0700

    - move the Javascript functions into JS files where they belong, but set   global URL variables through the JSP.

--- src/main/webapp/js/aceio.js
+++ src/main/webapp/js/aceio.js
@@ -2,7 +2,10 @@
  * Created by trenta on 23/01/14.
  */
 
-
+var aceLoginInfoUrl;
+var aceGetWeeksUrl;
+var aceGetWorkItemsUrl;
+var aceLoginUrl;
 var guid;
 
 var workList = [
@@ -87,15 +90,164 @@ jQuery(document).ready(function ()
     {
         log('work list: %o', workList);
         /* CRITICAL We could iterate through this list, to find a project and work
-        * item that matches the time frame, and just add to it's hours for the
+         * item that matches the time frame, and just add to it's hours for the
          * day of the week, as well as the comment. */
         if (0 < workList.length)
         {
             jQuery('#work').show();
         }
     });
+
+    // Check for existing login
+    guid = getCookie("fasttime");
+    log("Ace GUID: %s", guid);
+    if (guid !== undefined && guid.length == 36)
+    {
+        jQuery.ajax({
+            url: aceLoginInfoUrl,
+            type: 'get',
+            dataType: 'json',
+            data: 'guid=' + guid,
+            success: function (page, status, jqXHR)
+            {
+                aceLogin(page, status, jqXHR);
+            },
+            error: function (page, status, jqXHR)
+            {
+                aceIOError(page, status, jqXHR);
+            }
+        });
+    }
+
+    // Register login form button clicks for submitting the data through ajax.
+    jQuery('#login-submit').unbind('click.login').bind('click.login',
+            function (event)
+            {
+                log('submit clicked');
+                jQuery('#msg').replaceWith('<div id="msg"></div>');
+                var formData = jQuery("#fm1").serialize();
+                log('Form Data: %s', formData);
+                jQuery.ajax({
+                    url: aceLogin,
+                    type: 'post',
+                    dataType: 'json',
+                    data: formData,
+                    success: function (page, status, jqXHR)
+                    {
+                        aceLogin(page, status, jqXHR);
+                        location.reload();
+                    },
+                    error: function (page, status, jqXHR)
+                    {
+                        aceIOError(page, status, jqXHR);
+                    }
+                });
+                event.preventDefault();
+            });
 });
 
+/**
+ * Loads the in progress work list into workList global variable.
+ */
+function loadMyWorkItems(workWeek)
+{
+    for (i = 0; i < workWeek.results.length; i++)
+    {
+        jQuery.ajax({
+            url: aceGetWorkItemsUrl,
+            type: 'get',
+            dataType: 'json',
+            data: 'guid=' + guid + '&timeperiodid=' +
+                    workWeek.results[i].TIMESHEET_PERIOD_ID,
+            workListGenerator: true,
+            success: function (page, status, jqXHR)
+            {
+                for (i = 0; i < page.results.length; i++)
+                {
+                    var workItem = page.results[i];
+                    log('work item %o', workItem);
+                    workWeekStart =
+                            new Date(+/\/Date\((\d*)\)\//.exec(workItem.DATE_WEEK_START)[1] +
+                                    new Date().getTimezoneOffset() * 60 * 1000);
+                    log('date: %s', workWeekStart);
+                    var newWorkItem = {
+                        weekStart: workWeekStart,
+                        approvalStatus: workItem.APPROVAL_STATUS,
+                        approvalStatusName: workItem.APPROVAL_STATUS_NAME,
+                        project: workItem.PROJECT_ID,
+                        projectName: workItem.PROJECT_NAME,
+                        taskId: workItem.TASK_ID,
+                        taskName: workItem.TASK_RESUME,
+                        timeSheetLineId: workItem.TIMESHEET_LINE_ID,
+                        timeSheetPeriodId: workItem.TIMESHEET_PERIOD_ID,
+                        work: {
+                            sun: workItem.TOTAL1,
+                            mon: workItem.TOTAL2,
+                            tue: workItem.TOTAL3,
+                            wed: workItem.TOTAL4,
+                            thu: workItem.TOTAL5,
+                            fri: workItem.TOTAL6,
+                            sat: workItem.TOTAL7,
+                            total: workItem.TOTAL
+                        }
+                    };
+
+                    if (0 != newWorkItem.timeSheetLineId)
+                    { // only *real* time items, not the predicted ones.
+                        workList.push(newWorkItem);
+                        var workRow = '' +
+                                '            <tr>' +
+                                '            <td>' +
+                                newWorkItem.approvalStatusName +
+                                '</td>' +
+                                '            <td>' + newWorkItem.projectName +
+                                '</td>' +
+                                '            <td>' + newWorkItem.taskName +
+                                '</td>';
+                        workRow += '<td>'
+                        workRow += newWorkItem.work.sun + '-';
+                        workRow += newWorkItem.work.mon + '-';
+                        workRow += newWorkItem.work.tue + '-';
+                        workRow += newWorkItem.work.wed + '-';
+                        workRow += newWorkItem.work.thu + '-';
+                        workRow += newWorkItem.work.fri + '-';
+                        workRow += newWorkItem.work.sat;
+                        workRow += '</td>';
+                        workRow += '            </tr>'
+                        jQuery('#time').append(workRow);
+                    }
+                }
+            },
+            error: function (page, status, jqXHR)
+            {
+                aceIOError(page, status, jqXHR);
+            }
+        });
+    }
+}
+
+/**
+ * Loads the task items for each week.
+ */
+function loadMyWeeks()
+{
+    jQuery.ajax({
+        url: aceGetWeeksUrl,
+        type: 'get',
+        dataType: 'json',
+        data: 'guid=' + guid,
+        success: function (page, status, jqXHR)
+        {
+            log('my weeks: %o', page);
+            workList = [];
+            loadMyWorkItems(page);
+        },
+        error: function (page, status, jqXHR)
+        {
+            aceIOError(page, status, jqXHR);
+        }
+    });
+}
 /**
  * Pass a url, and a test call is created, and the results logged.  guid is
  * automatically appended.  This allows playing around in the chrome developer
@@ -104,18 +256,18 @@ jQuery(document).ready(function ()
 function testCall(queryUrl)
 {
     jQuery.ajax({
-      url: queryUrl,
-      type: 'get',
-      dataType: 'json',
-      data: 'guid=' + guid,
-      success: function (page, status, jqXHR)
-      {
-          log('test result: ', page);
-      },
-      error: function (page, status, jqXHR)
-      {
-        aceIOError(page, status, jqXHR);
-      }
+        url: queryUrl,
+        type: 'get',
+        dataType: 'json',
+        data: 'guid=' + guid,
+        success: function (page, status, jqXHR)
+        {
+            log('test result: ', page);
+        },
+        error: function (page, status, jqXHR)
+        {
+            aceIOError(page, status, jqXHR);
+        }
     });
 
 }
